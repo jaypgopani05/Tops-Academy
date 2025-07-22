2281a25d32d77c968a52d5c7ae65fc9dc9dab712
jaypgopani05 <jaypgopani05@gmail.com>
2025-07-22 22:19:39 +0530

    Delete Python directory
    Uploaded at wrong place.

--- Python/Assignment/Assignment -1/Python Program Files/Ans_15_String_Length.py
+++
@@ -1,8 +0,0 @@
-# Get input from the user
-text = input("Enter any string: ")
-
-# Use len() function to calculate length
-length = len(text)
-
-# Display the result
-print(f"The length of the string is: {length}")
